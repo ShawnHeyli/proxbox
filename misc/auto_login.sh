294a8c75aaaf244cc8b924d8271c2350f521315d
Hilan Meyran <hmeyran@univ-rennes.fr>
2023-10-31 10:58:21 -0400

    better auto-login.sh

--- misc/auto_login.sh
+++ misc/auto_login.sh
@@ -1,30 +1,8 @@
-override_getty="/etc/systemd/system/container-getty@1.service.d/override.conf"
-container_id=100
-
-pct exec $container_id -- bash -c "mkdir -p $(dirname $override_getty)"
-
 override="
 [Service]
 ExecStart=
-ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \$TERM
+ExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 $TERM
 "
 
-script="
-systemctl daemon-reload
-systemctl restart
-"
-
-pct exec $container_id -- bash -c "echo '$override' > $override_getty"
-pct exec $container_id -- bash -c "systemctl daemon-reload"
-pct exec $container_id -- bash -c "echo '$override' > $override_getty"
-
-
-let override_getty = "/etc/systemd/system/container-getty@1.service.d/override.conf";
-let dir_override_getty = "/etc/systemd/system/container-getty@1.service.d/override.conf";
-run_cmd!(pct exec 100 -- bash -c "mkdir -p $(dirname $override_getty)").unwrap();
-// Create a file
-let idk = "[Service]\nExecStart=\nExecStart=-/sbin/agetty --autologin root --noclear --keep-baud tty%I 115200,38400,9600 \\$TERM";
-fs::write("./auto_login", idk);
-run_cmd!(cp auto_login $override_getty).unwrap();
-run_cmd!(systemctl daemon-reload);
-run_cmd!(systemctl restart $override_getty);
+mkdir -p /etc/systemd/system/container-getty@1.service.d/
+echo $override >> /etc/systemd/system/container-getty@1.service.d/override.conf
